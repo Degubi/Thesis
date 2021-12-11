291722f3087abeaac37c10a429f58cbf25bf07f0
Degubi <gergonoorbi@gmail.com>
2021-12-11 15:54:23 +0100

    Add topNWords & topNWordsPerPOS stats

--- MagyarlancAnalyzer/Analyze.fsx
+++ MagyarlancAnalyzer/Analyze.fsx
@@ -1,16 +1,34 @@
 open System
 open System.IO
+open System.Text.Encodings.Web
 open System.Text.Json
 
-let createAnalysisStat filePath =
-    let stats = File.ReadLines(filePath) |> Seq.filter(fun k -> k <> String.Empty)
-                                         |> Seq.map(fun k -> k.Split('\t'))
-                                         |> Seq.map(fun k -> {| Word = k.[1]; PartOfSpeech = k.[3] |})
+type MagyarlancInfo = {| Word: string; PartOfSpeech: string |}
+
+let createTopNWordsStat(wordCount: int, stats: seq<MagyarlancInfo>) =
+    stats |> Seq.groupBy(fun k -> k.Word)
+          |> Seq.map(fun (word, stats) -> (word, {| count = stats |> Seq.length; pos = stats |> Seq.head |> fun k -> k.PartOfSpeech |}))
+          |> Seq.sortByDescending(fun (_, k) -> k.count)
+          |> Seq.truncate(wordCount)
+          |> dict
+
+let createTopWordsStatPerPOS(wordCount: int, stats: seq<MagyarlancInfo>) =
+    stats |> Seq.groupBy(fun k -> k.PartOfSpeech)
+          |> Seq.map(fun (pos, stats) -> (pos, stats |> Seq.countBy(fun k -> k.Word) |> Seq.sortByDescending(fun (_, frequency) -> frequency) |> Seq.truncate(wordCount) |> dict))
+          |> dict
+
+let createAnalysisStat(filePath: string) =
+    let fileStats = File.ReadLines(filePath) |> Seq.filter(fun k -> k <> String.Empty)
+                                             |> Seq.map(fun k -> k.Split('\t'))
+                                             |> Seq.map(fun k -> {| Word = k.[1]; PartOfSpeech = k.[3] |})
     {|
-        POSCounts = stats |> Seq.countBy(fun k -> k.PartOfSpeech) |> dict
+        posCounts = fileStats |> Seq.countBy(fun k -> k.PartOfSpeech) |> dict
+        topWords = createTopNWordsStat(5, fileStats)
+        topWordsPerPOS = createTopWordsStatPerPOS(5, fileStats)
     |}
 
-let outputOptions = JsonSerializerOptions(WriteIndented = true)
+
+let outputOptions = JsonSerializerOptions(WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping)
 
 if Directory.Exists("analyze_outputs") then
     Directory.Delete("analyze_outputs", true)
