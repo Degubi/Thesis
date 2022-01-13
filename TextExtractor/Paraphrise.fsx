d8ffe3e42755893c036d8fa7f2de4185d8835811
Degubi <gergonoorbi@gmail.com>
2022-01-13 14:22:15 +0100

    Some f# syntax refactoring

--- TextExtractor/Paraphrise.fsx
+++ TextExtractor/Paraphrise.fsx
@@ -2,7 +2,7 @@ open System
 open System.IO
 
 let paraphriseFileContent filePath =
-    File.ReadAllText(filePath) |> fun k -> k.Split("\n\n")
+    File.ReadAllText(filePath) |> fun k -> k.Split "\n\n"
                                |> Seq.map(fun k -> k.Replace("-\n", "").Replace("\n", " "))
                                |> fun k -> String.Join("\n", k)
 
@@ -10,6 +10,6 @@ let mainDir = Directory.GetParent(__SOURCE_DIRECTORY__).FullName
 let inputDir = $"{mainDir}/outputs/text/raw"
 let outputDir = $"{mainDir}/outputs/text/paraphrised"
 
-Directory.CreateDirectory(outputDir)
-Directory.GetFiles(inputDir) |> Seq.map(fun k -> (k, paraphriseFileContent(k)))
-                             |> Seq.iter(fun (path, content) -> File.WriteAllText($"{outputDir}/{Path.GetFileName(path)}", content))
+Directory.CreateDirectory outputDir
+Directory.GetFiles(inputDir) |> Seq.map(fun k -> (k, paraphriseFileContent k))
+                             |> Seq.iter(fun (path, content) -> File.WriteAllText($"{outputDir}/{Path.GetFileName path}", content))
