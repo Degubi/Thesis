7cfd944950a087a0dccf07fa7d41b40a9b40976e
Degubi <gergonoorbi@gmail.com>
2025-03-19 19:49:43 +0100

    Update book pdf links in setup.py

--- setup.py
+++ setup.py
@@ -24,17 +24,17 @@ download_file_to('https://rgai.inf.u-szeged.hu/sites/rgai.inf.u-szeged.hu/files/
 download_file_to('https://raw.githubusercontent.com/tesseract-ocr/tessdata/main/hun.traineddata', 'TextExtractor/tessdata/hun.traineddata')
 download_file_to('https://raw.githubusercontent.com/tesseract-ocr/tessdata/main/osd.traineddata', 'TextExtractor/tessdata/osd.traineddata')
 
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/OH-MIR09SZ__teljes.pdf', 'TextExtractor/pdfs/9_szgy.pdf')
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/OH-MIR09TA__teljes.pdf', 'TextExtractor/pdfs/9_tk.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/OH-MIR09SZ__teljes.pdf', 'TextExtractor/pdfs/9_szgy.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/OH-MIR09TA__teljes.pdf', 'TextExtractor/pdfs/9_tk.pdf')
 
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021002_1__teljes.pdf', 'TextExtractor/pdfs/10_szgy.pdf')
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021001_1__teljes.pdf', 'TextExtractor/pdfs/10_tk.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021002_1__teljes.pdf', 'TextExtractor/pdfs/10_szgy.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021001_1__teljes.pdf', 'TextExtractor/pdfs/10_tk.pdf')
 
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021102_1__teljes.pdf', 'TextExtractor/pdfs/11_szgy.pdf')
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021101_1__teljes.pdf', 'TextExtractor/pdfs/11_tk.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021102_1__teljes.pdf', 'TextExtractor/pdfs/11_szgy.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021101_1__teljes.pdf', 'TextExtractor/pdfs/11_tk.pdf')
 
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021202_1__teljes.pdf', 'TextExtractor/pdfs/12_szgy.pdf')
-download_file_to('https://www.tankonyvkatalogus.hu/pdf/FI-501021201_1__teljes.pdf', 'TextExtractor/pdfs/12_tk.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021202_1__teljes.pdf', 'TextExtractor/pdfs/12_szgy.pdf')
+download_file_to('https://www.tankonyvkatalogus.hu/storage/pdf/FI-501021201_1__teljes.pdf', 'TextExtractor/pdfs/12_tk.pdf')
 
 call('mvn install:install-file -Dfile=magyarlanc.jar -DgeneratePom=true -Dversion=3.0 -Dpackaging=jar -DgroupId=rgai.inf.u.szeged.hu -DartifactId=magyarlanc -Dsources=magyarlanc_src.zip')
 remove('magyarlanc.jar')
